3b160faea06b40fde1a57c50a83f9334b06b1feb
ayush24k <ayushanshu69@gmail.com>
2025-01-06 23:46:00 +0530

    setup toast comp

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,4 +1,26 @@
+'use client'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from 'zod'
+import Link from "next/link"
+import { useState } from "react"
+import { useDebounceValue } from 'usehooks-ts'
+import { useToast } from "@/hooks/use-toast"
+import { useRouter } from "next/navigation"
+
+
+
 export default function SignInPage () {
+    const [username, setUsername] = useState("");
+    const [usernameMessage, setUsrnameMessage] = useState("");
+    const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const debouncedUsername = useDebounceValue(username, 300);
+    const {toast} = useToast();
+    const router = useRouter();
+
+    
     return (
         <div>
             sign-in Page
