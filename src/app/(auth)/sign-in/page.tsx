45a68492294f81359a588c58b8ec40a25f4af422
ayush24k <ayushanshu69@gmail.com>
2025-01-13 00:52:47 +0530

    added form logic

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,10 +3,13 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from 'zod'
 import Link from "next/link"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useDebounceValue } from 'usehooks-ts'
 import { useToast } from "@/hooks/use-toast"
 import { useRouter } from "next/navigation"
+import { signUpSchema } from "@/ZodValidation/signUpSchema"
+import axios, {AxiosError} from "axios"
+import { ApiResponse } from "@/types/ApiResponse"
 
 
 
@@ -20,7 +23,60 @@ export default function SignInPage () {
     const {toast} = useToast();
     const router = useRouter();
 
-    
+
+    // zod validation
+    const form = useForm<z.infer<typeof signUpSchema>>({
+        resolver: zodResolver(signUpSchema),
+        defaultValues: {
+            username: "",
+            email: "",
+            password: "",
+        }
+    });
+
+    useEffect(() => {
+        const checkUsernameUnique = async () => {
+            if (debouncedUsername) {
+                setIsCheckingUsername(true)
+                setUsrnameMessage("")
+
+                try{
+                    const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`);
+                    setUsrnameMessage(response.data.message);
+                } catch (err) {
+                    const axiosError = err as AxiosError<ApiResponse>
+                    setUsrnameMessage(axiosError.response?.data.message || "Error checking username")
+                } finally {
+                    setIsCheckingUsername(false);
+                }
+            }
+        } 
+    }, [debouncedUsername])
+
+    const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+        setIsSubmitting(true);
+        try {
+            const response = await axios.post<ApiResponse>('/api/sign-up', data);
+            toast({
+                title: 'success',
+                description: response.data.message 
+            })
+            router.replace(`/verify/${username}`)
+        } catch (error) {
+            console.error("Error in signup of user", error);
+            const axiosError = error as AxiosError<ApiResponse>
+            let errorMessage = axiosError.response?.data.message;
+
+            toast({
+                title: "Signup Failed",
+                description: errorMessage,
+                variant: "destructive"
+            })
+        } finally {
+            setIsSubmitting(false)
+        }
+    }
+
     return (
         <div>
             sign-in Page
